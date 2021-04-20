8bbbe480469e6594c8321368358c624365353261
Varun Kalidindi <varunram64@gmail.com>
2021-04-20 09:52:43 +0530

    Delete post functionality.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = "FETCH_POSTS";
 export const CREATE_POST = "CREATE_POST";
 export const FETCH_POST = "FETCH_POST";
+export const DELETE_POST = "DELETE_POST";
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY="?key=VARUNRAM1234";
 
@@ -31,5 +32,15 @@ export function fetchPost(id) {
     return {
         type: FETCH_POST,
         payload: request
+    };
+}
+
+export function deletePost(id, callBack) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callBack());
+
+    return {
+        type: DELETE_POST,
+        payload: id
     };
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
         const { id } = this.props.match.params;
         this.props.fetchPost(id);
+    }
+
+    onDeleteClick = () => {
+        const { id } = this.props.match.params;
+        this.props.deletePost(id, () => {
+            this.props.history.push("/");
+        });
     }
 
     render() {
@@ -20,6 +27,10 @@ class PostsShow extends Component {
         return (
         <div>
             <Link to="/">Back to Index</Link>
+            <button className="btn btn-danger pull-xs-right"
+                    onClick={this.onDeleteClick}>
+                Delete
+            </button>
             <h3>{post.title}</h3>
             <h6>Categories: {post.categories}</h6>
             <p>{post.content}</p>
@@ -34,4 +45,4 @@ function mapStateToProps({ posts }, ownProps) {
     };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
